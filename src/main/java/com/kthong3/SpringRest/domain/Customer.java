7acbc1cc6bb96d0da3fd75b8f64110e0aa46d2cc
kthong3 <kthongsakounh@gmail.com>
2018-09-26 10:29:46 -0500

    create customer class using JPA and Lombok

---
+++ src/main/java/com/kthong3/SpringRest/domain/Customer.java
@@ -0,0 +1,17 @@
+package com.kthong3.SpringRest.domain;
+
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
