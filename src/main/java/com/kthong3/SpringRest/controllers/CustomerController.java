ee489e993bde857ba13d1c77a23702c455f73441
kthong3 <kthongsakounh@gmail.com>
2018-09-26 14:30:51 -0500

    add PostMapping to CustomerController to create new customer object and persist in the database.

--- src/main/java/com/kthong3/SpringRest/controllers/CustomerController.java
+++ src/main/java/com/kthong3/SpringRest/controllers/CustomerController.java
@@ -2,9 +2,13 @@ package com.kthong3.SpringRest.controllers;
 
 import com.kthong3.SpringRest.domain.Customer;
 import com.kthong3.SpringRest.services.CustomerService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -28,5 +32,11 @@ public class CustomerController {
     @GetMapping("/{id}")
     public Customer getCustomerById(@PathVariable Long id){
         return customerService.findCustomerById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer saveCustomer(@RequestBody Customer customer){
+        return customerService.saveCustomer(customer);
     }
 }
--- src/main/java/com/kthong3/SpringRest/services/CustomerServiceImpl.java
+++ src/main/java/com/kthong3/SpringRest/services/CustomerServiceImpl.java
@@ -24,4 +24,9 @@ public class CustomerServiceImpl implements CustomerService {
     public List<Customer> findAllCustomers() {
         return customerRepository.findAll();
     }
+
+    @Override
+    public Customer saveCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
 }
